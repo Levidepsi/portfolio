694fbf3520717d2102c59227a672c21e82de5c39
ArvinDev02 <garcellanoarvin@gmail.com>
2025-01-09 15:10:16 +0800

    initial commit

--- components/sections/TextWithImage.tsx
+++ components/sections/TextWithImage.tsx
@@ -9,9 +9,9 @@ const TextWithImage = ({ textwimage_items, padding_top }:any) => {
       {textwimage_items && textwimage_items.map((item: any, index: number) => {
         console.log(item.image.asset)
         return (
-          <div className={`flex  flex-col-reverse ${item.content_position == true ? "lg:flex-row-reverse" : "lg:flex-row"}`} key={index}>
+          <div className={`flex flex-col-reverse ${item.content_position == true ? "lg:flex-row-reverse" : "lg:flex-row"}`} key={index}>
             <div className={`lg:w-[50%] ${item.content_position == true ? "flex justify-end" : ""}`}>
-              <div className=" lg:flex flex-col h-full justify-between">
+              <div className={`lg:flex flex-col h-full justify-between ${item.body == null ? "justify-end" : "justify-between"}`}>
               <h1 className="text-[#30282A] text-[65px] lg:max-w-[480px] tracking-[1.3px] leading-[56px] boing_thin mb-5">{item.title}</h1>
               <div className="avenir_book [&>p]:text-[#30282A] lg:max-w-[450px] text-[15px] tracking-[0.3px] leading-[22px]"><PortableText value={item.body}/></div>
             </div>  

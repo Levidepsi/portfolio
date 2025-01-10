375d213894cc18c817bf145f4a3ab24a7053071a
ArvinDev02 <garcellanoarvin@gmail.com>
2025-01-10 15:32:29 +0800

    timeline component

--- components/sections/Timeline.tsx
+++ components/sections/Timeline.tsx
@@ -13,20 +13,15 @@ const Timeline = ({ title, timeline_items, background }: any) => {
       )}
       <div className="md:flex md:flex-wrap md:justify-start md:gap-y-[25px]">
         {timeline_items && timeline_items.map((item: any, index: number) => {
-          // console.log(item.slug.slug)
+            console.log(item) 
             return (
-            <div className={`text_items item${index} flex justify-between md:w-[45%] lg:w-[33.33%] md:items-center flex-col text-center lg:flex-col py-[17px]`} key={index}>
+            <div className={`timeline-item w-[50%] flex justify-between md:w-[45%] lg:w-[33.33%] md:items-center flex-col text-center lg:flex-col py-[17px]`} key={index}>
                 <div className="text-[13px] avenir_roman text-[#30282A] tracking-[1.56px] leading-[18px] lg:w-[50%] mb-4">
-                    <div className="title_icon">
-                        {item.image && (
-                            <Image src={item.image} alt={item.title} width={2000} height={2000} priority className="object-contain mb-4 max-w-[50px] mx-[auto]" />
-                        )}
-                    </div>
-                    <PortableText value={item.title} />
+                    {item.title}
                 </div>
-                <div className="text-[13px] avenir_roman [&>p]:text-[#30282A] pb-[10px] tracking-[0.26px] leading-[18px] lg:w-[80%] "><PortableText value={item.body} /></div>
-                <Link href={`${item.slug != null ? `/companies/${item.slug.slug}` : ""}`} className="text-[13px] avenir_roman text-[#30282A] tracking-[0.26px] leading-[18px] lg:w-[80%] ">{item.learn_more}</Link>
-                
+                {item.body && (
+                    <div className="text-[13px] avenir_roman [&>p]:text-[#30282A] pb-[10px] tracking-[0.26px] leading-[18px] lg:w-[80%] "><PortableText value={item.body} /></div>
+                )}
             </div>
             )
         })}
